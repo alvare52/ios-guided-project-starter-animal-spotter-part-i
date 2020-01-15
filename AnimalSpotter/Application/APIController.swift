1200537645bc4feaa11f49851991045c3a144131
Jorge Alvarez <jorgelalvarez52@gmail.com>
2020-01-15 13:04:41 -0800

    ?

--- AnimalSpotter/Application/APIController.swift
+++ AnimalSpotter/Application/APIController.swift
@@ -17,10 +17,99 @@ enum HTTPMethod: String {
 class APIController {
     
     private let baseUrl = URL(string: "https://lambdaanimalspotter.vapor.cloud/api")!
+    var bearer: Bearer?
+    
+    var isUserLoggedIn: Bool {
+        if bearer == nil {
+            return false
+        } else {
+            return true
+        }
+    }
     
     // create function for sign up
+    func signUp(user: User, completion: @escaping (Error?) -> Void) {
+        let signUPUrl = baseUrl.appendingPathComponent("users/signup") //  adds / for you
+        
+        var request = URLRequest(url: signUPUrl)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        // Body
+        let jsonEncoder = JSONEncoder()
+        do {
+            request.httpBody = try jsonEncoder.encode(user)
+        } catch {
+            print("Error encoding user object: \(error)")
+            completion(error)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { _, response, error in
+            if let response = response as? HTTPURLResponse, response.statusCode != 200 {
+                completion(NSError(domain: "", code: response.statusCode, userInfo: nil))
+                return
+            }
+            
+            if let error = error {
+                completion(error)
+                return
+            }
+            
+            // request processed and successful
+            
+            completion(nil) // no error 
+        }.resume()
+    }
     
     // create function for sign in
+    func signIn(user: User, completion: @escaping (Error?) -> Void) {
+        let signInUrl = baseUrl.appendingPathComponent("users/login") //  adds / for you
+        
+        var request = URLRequest(url: signInUrl)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        // Body
+        let jsonEncoder = JSONEncoder()
+        do {
+            request.httpBody = try jsonEncoder.encode(user)
+        } catch {
+            print("Error encoding user object: \(error)")
+            completion(error)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let response = response as? HTTPURLResponse, response.statusCode != 200 {
+                completion(NSError(domain: "", code: response.statusCode, userInfo: nil))
+                return
+            }
+            
+            if let error = error {
+                completion(error)
+                return
+            }
+            
+            guard let data = data else {
+                completion(NSError())
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            do {
+                self.bearer = try decoder.decode(Bearer.self, from: data)
+            } catch {
+                print("Error decoding bearer object: \(error)")
+                completion(error)
+                return
+            }
+            
+            // request processed and successful
+            
+            completion(nil) // no error
+        }.resume()
+    }
     
     // create function for fetching all animal names
     
---
+++ AnimalSpotter/Models/Bearer.swift
@@ -0,0 +1,13 @@
+//
+//  Bearer.swift
+//  AnimalSpotter
+//
+//  Created by Jorge Alvarez on 1/15/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Bearer: Codable {
+    let token: String
+}
